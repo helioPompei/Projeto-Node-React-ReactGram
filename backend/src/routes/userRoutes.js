b31996e99dba6166b62f3f07dbe6d4dd8f73adec
helioPompei <helioberizonzipompei2017@gmail.com>
2023-05-28 00:25:59 -0300

    feat: create validation middlewares to user

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 // Import express
 import express from "express";
 const userRouter = express.Router();
+
 // Import controller
 import { register } from "../controllers/UserController.js";
 
+// Middlewares
+import validate from "../middlewares/handleValidation.js";
+import userCreateValidation from "../middlewares/userValidations.js";
+
 // User Routes
-userRouter.get("/register", register);
+userRouter.get("/register", userCreateValidation(), validate, register);
 
 export default userRouter;
